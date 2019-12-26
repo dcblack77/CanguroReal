a6dea2513cde477b50200fbaa4bb823646567545
Diego Daniel Cabrera Rojas <diegocabrera123@gmail.com>
2019-12-26 20:27:17 +0100

    subida a github

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+//Install express server
+require("./config/");
+require("./config/db");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const api = require("./Routes");
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse application/json
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '../dist/RiojaCanguro'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '../dist/RiojaCanguro/index.html'));
+});
+//Configuración Global de rutas
+
+app.use("/api", api);
+
+async function main() {
+    await app.listen(process.env.PORT);
+    console.log("Server on port", process.env.PORT);
+}
+
+main();
